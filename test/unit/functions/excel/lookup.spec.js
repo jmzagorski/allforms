a573d2e63dd48180fc0a973ab58ef738de10eb2a
Jeremy Zagorski <jmzagorski@gmail.com>
2017-02-23 14:41:13 -0500

    test(macro): add missing LOOKUP macro test

--- test/unit/functions/excel/lookup.spec.js
+++ test/unit/functions/excel/lookup.spec.js
@@ -1,22 +1,67 @@
-import { xl_lookup } from '../../../../src/functions/excel'
+import '../../setup';
+import { HttpClient } from 'aurelia-fetch-client';
+import { Lookup } from '../../../../src/functions/excel/macros';
+import { Parser } from 'hot-formula-parser';
+import using from 'jasmine-data-provider';
 
-describe('the excel lookup function', () => {
+describe('the excel lookup macro', () => {
+  let sut;
+  let httpSpy;
+  let parserSpy;
 
-  it('calls ajax request', () => {
-    /* arrange */
-    const segments = ['api', '1', '2'];
-    const lookupReturn = '4';
+  beforeEach(() => {
+    httpSpy = jasmine.setupSpy('http', HttpClient.prototype);
+    parserSpy = jasmine.setupSpy('parser', Parser.prototype);
+    sut = new Lookup(httpSpy, parserSpy);
+  });
 
-    const ajaxSpy = spyOn($, 'ajax')
+  it('configures a new http client', () => {
+    let isStandard = false;
+    let pFunc = null;
+    let config = {
+      useStandardConfiguration: () => isStandard = true
+    };
+    httpSpy.configure.and.callFake(func => pFunc = func);
 
-    /* act */
-    const actual = xl_lookup(segments);
+    const psut = new Lookup(httpSpy);
+    pFunc(config);
 
-    /* assert */
-    expect(ajaxSpy).toHaveBeenCalledWith({
-      cache: true,
-      type: 'get',
-      url: 'api/1/2'
+    expect(config).not.toEqual(null);
+    expect(isStandard).toBeTruthy();
+  });
+
+  it('calls the apis to get the lookup data', async done => {
+    const lookupObj = { a: 1 };
+    const returnName = 'b';
+    const metadata = { api: 'c' };
+    httpSpy.fetch.and.returnValues(Promise.resolve(metadata), Promise.resolve({}));
+
+    await sut.transform(lookupObj, returnName);
+
+    expect(httpSpy.fetch.calls.argsFor(0)).toEqual(['lookups/a']);
+    expect(httpSpy.fetch.calls.argsFor(1)).toEqual(['c/1']);
+    done();
+  });
+
+  using([
+    { table: [[1,2]], newTable: [[1,2],[4,9]] },
+    { table: undefined, newTable: [[4,9]] }
+  ], data => {
+    it('transforms the lookup macro into vlookup', async done => {
+      const lookupObj = { a: 1 };
+      const returnName = 'b';
+      const retObj = { c: 4, b: 9 };
+      const metadata = { api: '' };
+
+      httpSpy.fetch.and.returnValues(Promise.resolve(metadata), Promise.resolve(retObj));
+      parserSpy.getVariable.and.returnValue(data.table);
+
+      const formula = await sut.transform(lookupObj, returnName);
+
+      expect(formula).toEqual('VLOOKUP(1, atable, 1, true)');
+      expect(parserSpy.getVariable).toHaveBeenCalledWith('atable');
+      expect(parserSpy.setVariable).toHaveBeenCalledWith('atable', data.newTable);
+      done();
     });
   });
 
