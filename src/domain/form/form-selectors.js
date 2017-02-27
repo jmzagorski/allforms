0ec52fea65090b62a01322001063706b2c1525f9
Jeremy Zagorski <jmzagorski@gmail.com>
2017-02-27 10:09:18 -0500

    refactor(forms): add more detailed form selector for history

--- src/domain/form/form-selectors.js
+++ src/domain/form/form-selectors.js
@@ -7,3 +7,25 @@ export function getActiveForm(state) {
 export function getFormList(state) {
   return state.forms.list;
 }
+
+/**
+ * @summary selects the active forms most recent history per file
+ * @param {Object} state the state of the app
+ * @return {Array<Object>} a new array with the forms most recent history
+ *
+ */
+export function getRecentFormHistory(state) {
+  const form = getActiveForm(state);
+
+  return form.files
+    .map(file => {
+      return {
+        name: file.name,
+        formId: form.id,
+        formName: form.name,
+        comment: file.lastComment,
+        revisedDays: file.lastEditInDays,
+        icon: file.icon
+      };
+    })
+}
--- test/unit/domain/form/form-selectors.spec.js
+++ test/unit/domain/form/form-selectors.spec.js
@@ -6,9 +6,21 @@ describe('the form selectors', () => {
   let forms;
 
   beforeEach(() => {
-    forms = [
-      { id: 'a', files: [ { priority: 7 }, { priority: 4 } ]},
-      { id: 'b', files: [ { priority: 2 }, { priority: 1 } ]}
+    forms = [{
+      id: 'a',
+      name: 'aa',
+      files: [
+        { name: 'c', priority: 7, lastComment: 'zas', lastEditInDays: 4, icon: 'p' },
+        { name: 'd', priority: 4, lastComment: 'zax', lastEditInDays: 5, icon: 't' }
+      ]
+    }, {
+      id: 'b',
+      name: 'bb',
+      files: [
+        { name: 'e', priority: 2, lastComment: 'ax', lastEditInDays: 6, icon: 'b' },
+        { name: 'f', priority: 1, lastComment: 'ayy', lastEditInDays: 7, icon: 'q' }
+      ]
+    }
     ];
     state = {
       forms: {
@@ -35,5 +47,16 @@ describe('the form selectors', () => {
     const actualForms = selectors.getFormList(state);
 
     expect(actualForms).toBe(forms)
+  });
+
+  it('returns the forms recent history', () => {
+    const actualHistory = selectors.getRecentFormHistory(state);
+
+    expect(actualHistory).toContain({
+      name: 'c', formId: 'a', formName: 'aa', comment: 'zas', revisedDays: 4, icon: 'p'
+    });
+    expect(actualHistory).toContain({
+      name: 'd', formId: 'a', formName: 'aa', comment: 'zax', revisedDays: 5, icon: 't'
+    });
   });
 });
