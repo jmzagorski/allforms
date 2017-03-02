e405fdd77777d9802924eec434ebdc5b1a2ab156
Jeremy Zagorski <jmzagorski@gmail.com>
2017-03-02 10:42:04 -0500

    doc(renderers): add interface file for all renderers to implement for factory wrapper

---
+++ src/renderers/interface.js
@@ -0,0 +1,20 @@
+/**
+ * @interface Renderer
+ */
+
+/**
+ * @summary creates a new element
+ * @function
+ * @name Renderer#create(options)
+ * @param {Object} options key/value object to set values on new element
+ * @returns {HtmlElement} the new element
+ */
+
+/**
+ * @summary updates an existing element
+ * @function
+ * @name Renderer#update(element, options)
+ * @param {HtmlElement} element the element to update
+ * @param {Object} options key/value object to set values on new element
+ * @return {HtmlElement} the updated element
+ */
