b486612049b33dc8f1423ad0ad33967f925c8278
Jeremy Zagorski <jmzagorski@gmail.com>
2017-03-07 10:20:10 -0500

    feat(formulas): add element names to function array for user to select
    help create formulas with textcomplete for excel functions and variables

--- src/formulas.js
+++ src/formulas.js
@@ -1,18 +1,25 @@
 import { ExcelEngine } from './functions/excel/engine';
+import { TemplateApi } from './api/template-api';
 
 export class Formulas {
-  static inject() { return [ ExcelEngine ]; }
+  static inject() { return [ ExcelEngine, TemplateApi ]; }
 
-  constructor(xl) {
+  constructor(xl, templateApi) {
     this.funcNames = xl.functions;
     this.verified = false;
     this.verifyMessage = null;
 
     this._xl = xl;
+    this._templateApi = templateApi;
   }
 
-  activate(model) {
+  async activate(model) {
     this.element = model;
+    const $elements = this._templateApi.find(model.formId).elements;
+
+    for (let i = 0; i < $elements.length; i++) {
+      this.funcNames.push($elements[i].name);
+    }
   }
 
   async verify() {
--- test/unit/formulas.spec.js
+++ test/unit/formulas.spec.js
@@ -1,31 +1,49 @@
 import './setup';
 import { Formulas } from '../../src/formulas';
 import { ExcelEngine } from '../../src/functions/excel/engine';
+import { TemplateApi } from '../../src/api/template-api';
 import using from 'jasmine-data-provider';
 
 describe('the formulas view model', () => {
   let sut;
   let xlSpy;
+  let templApiSpy;
 
   beforeEach(() => {
     xlSpy = jasmine.setupSpy('xl', ExcelEngine.prototype);
+    templApiSpy = jasmine.setupSpy('templApi', TemplateApi.prototype);
 
-    xlSpy.functions = 1;
-    sut = new Formulas(xlSpy);
+    xlSpy.functions = [ 1 ];
+    sut = new Formulas(xlSpy, templApiSpy);
+
+    templApiSpy.find.and.returnValue({ elements: [] });
   });
 
   it('instantiates the view model', () => {
-    expect(sut.funcNames).toEqual(1);
+    expect(sut.funcNames).toEqual([1]);
     expect(sut.verified).toBeFalsy();
     expect(sut.verifyMessage).toEqual(null);
   });
 
-  it('sets the element to the model on activate', () => {
+  it('sets the element to the model on activate', async done => {
     const element = {};
 
-    sut.activate(element);
+    await sut.activate(element);
 
     expect(sut.element).toBe(element);
+    done();
+  });
+
+  it('pushes the template elements into the func names', async done => {
+    const elements = [{ name: 'a' }, { name: 'b' }];
+    templApiSpy.find.and.returnValue({ elements })
+
+    await sut.activate({ formId: 1 });
+
+    expect(sut.funcNames).toContain('a')
+    expect(sut.funcNames).toContain('b')
+    expect(templApiSpy.find).toHaveBeenCalledWith(1);
+    done();
   });
 
   using([
