340a8f000ed29c6fac924bb91a745aca2902cc71
Jeremy Zagorski <jmzagorski@gmail.com>
2017-03-20 14:10:39 -0400

    feat(formdata): add formData api

---
+++ src/api/form-data.js
@@ -0,0 +1,58 @@
+import { HttpClient, json } from 'aurelia-fetch-client';
+import { importFetch } from '../utils';
+
+const fetch = importFetch(); // eslint-disable-line no-unused-vars
+
+/**
+ * @summary RESTFUL api for interact with the form data backend
+ * @implements {IFormDataApi}
+ */
+export class FormDataApi {
+
+  static inject = [ HttpClient ];
+
+  /**
+   * @summary instantiates the FormDataApi with a fetch client
+   * @param {Object} http the http fetch object
+   * @param {Object} http.fetch the method to call a RESTFUL api
+   */
+  constructor(http) {
+    this._http = http;
+  }
+
+  /**
+   * @summary Gets an array of the master form's IFormDataSummary objects
+   * @param {string} formId the id of the master form
+   * @return {Promise<IFormDataSummary[]>} a promise to return IFormDataSummary
+   * array
+   */
+  getAll(formId) {
+    return this._http.fetch(`forms/${formId}/form-data`)
+      .then(response => response.json());
+  }
+
+  /**
+   * @summary Gets an single IFormData object
+   * @param {number} formDataId the id of the IFormData object
+   * @return {Promise<IFormData>} a promise to return the IFormData object
+   */
+  get(formDataId) {
+    return this._http.fetch(`form-data/${formDataId}`)
+      .then(response => response.json());
+  }
+
+  /**
+   * @summary Saves a single IFormDataObject
+   * @param {IFormData} formData the id of the IFormData object
+   * @return {Promise<IFormData>} a promise to return the IFormData object
+   */
+  save(formData) {
+    const url = formData.id ? `form-data/${formData.id}` : 'form-data';
+    const method = formData.id ? 'PUT' : 'POST';
+
+    return this._http.fetch(url, {
+      method: method,
+      body: json(formData)
+    }).then(response => response.json());
+  }
+}
---
+++ test/unit/api/form-data.spec.js
@@ -0,0 +1,60 @@
+import { HttpStub } from '../stubs';
+import { FormDataApi } from '../../../src/api/form-data';
+
+describe('the form data api', () => {
+  let sut;
+  let httpStub;
+
+  beforeEach(() => {
+    httpStub = new HttpStub();
+    sut = new FormDataApi(httpStub);
+  });
+
+  it('fetches all the form data list object by the master form id', async done => {
+    httpStub.itemStub = [];
+
+    const actual = await sut.getAll(1);
+
+    expect(httpStub.url).toEqual('forms/1/form-data');
+    expect(actual).toBe(httpStub.itemStub);
+    done();
+  });
+
+  it('fetches a single form data object by the id', async done => {
+    httpStub.itemStub = {};
+
+    const actual = await sut.get(1);
+
+    expect(httpStub.url).toEqual('form-data/1');
+    expect(actual).toBe(httpStub.itemStub);
+    done();
+  });
+
+  using([
+    { id: null, method: 'POST', url: 'form-data' },
+    { id: undefined, method: 'POST', url: 'form-data' },
+    { id: 0, method: 'POST', url: 'form-data' },
+    { id: 1, method: 'PUT', url: 'form-data/1' }
+  ], data => {
+    it('saves the form data based on the id', async done => {
+      const formData = { id: data.id, formName: 'test' }
+      const returnedData = {};
+      const fr = new FileReader();
+
+      httpStub.itemStub = returnedData;
+
+      fr.addEventListener('loadend', () => {
+        expect(fr.result).toEqual(JSON.stringify(formData));
+        done();
+      });
+
+      const actual = await sut.save(formData);
+
+      expect(httpStub.url).toEqual(data.url);
+      expect(httpStub.blob.method).toEqual(data.method);
+      expect(actual).not.toBe(formData);
+      expect(actual).toBe(returnedData);
+      fr.readAsText(httpStub.blob.body);
+    });
+  });
+});
