8e42a1e2a2a2eee11eb31e184b2390e3a8668c49
Jeremy Zagorski <jmzagorski@gmail.com>
2017-03-22 12:47:48 -0400

    refactor(signaler): hook signaler into aurelia app

--- src/main.js
+++ src/main.js
@@ -8,6 +8,8 @@ import { createStore, applyMiddleware } from 'redux';
 import { HttpClient } from 'aurelia-fetch-client';
 import { getBaseUrl } from './env';
 import { PLATFORM } from 'aurelia-pal';
+import { BindingMiddleware } from './binding-middleware';
+import { BindingSignaler } from 'aurelia-templating-resources';
 import createSagaMiddleware from 'redux-saga';
 import rootReducer from './root-reducer';
 import setupSaga from './root-saga';
@@ -21,6 +23,8 @@ Bluebird.config({ warnings: false });
 export async function configure(aurelia) {
   const sagaMiddleware = createSagaMiddleware();
   const http = new HttpClient();
+  const signaler = new BindingSignaler();
+  const signalerMiddleware = new BindingMiddleware(signaler);
 
   aurelia.use
     .standardConfiguration()
@@ -28,7 +32,7 @@ export async function configure(aurelia) {
     .plugin('aurelia-dialog')
     .plugin('aurelia-value-converters')
     .plugin('aurelia-redux-plugin', {
-      store: createStore(rootReducer, applyMiddleware(sagaMiddleware))
+      store: createStore(rootReducer,applyMiddleware(sagaMiddleware, signalerMiddleware.listen))
     });
 
   configureHttp(http, getBaseUrl(PLATFORM.location));
@@ -37,6 +41,7 @@ export async function configure(aurelia) {
 
   aurelia.container.registerInstance(Interact, Interact);
   aurelia.container.registerInstance(HttpClient, http);
+  aurelia.container.registerInstance(BindingSignaler, signaler);
 
   for (let macro in macros) {
     const type = Object.prototype.toString.call(macros[macro]);
