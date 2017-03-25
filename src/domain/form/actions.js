2ffd4873832d2fa8cd2cb69b0a2ac91b05f89677
Jeremy Zagorski <jmzagorski@gmail.com>
2017-03-25 18:19:21 -0400

    refactor(form): remove old form actions

--- src/domain/form/actions.js
+++ src/domain/form/actions.js
@@ -1,17 +1,9 @@
-import { FormApi } from '../../api/form-api';
-import { Store } from 'aurelia-redux-plugin';
-
 export const REQUEST_FORM = 'REQUEST_FORM';
 export const RECEIVED_FORM = 'RECEIVED_FORM';
 export const CREATE_FORM = 'CREATE_FORM';
 export const FORM_CREATED = 'FORM_CREATED';
 export const EDIT_FORM = 'EDIT_FORM';
 export const FORM_EDITED = 'FORM_EDITED';
-
-export const LOAD_FORMS_SUCCESS = 'LOAD_FORMS_SUCCESS';
-export const ADD_FORM_SUCCESS = 'ADD_FORM_SUCCESS';
-export const EDIT_FORM_SUCCESS = 'EDIT_FORM_SUCCESS';
-export const ACTIVATE_FORM_SUCCESS = 'ACTIVATE_FORM_SUCCESS';
 
 export function requestForm(id) {
   return {
@@ -55,52 +47,5 @@ export function formEdited(data, hasError) {
     type: FORM_EDITED,
     payload: data,
     error: hasError
-  }
-}
-
-function loadFormsSuccess(forms) {
-  return { type: LOAD_FORMS_SUCCESS, forms };
-}
-
-function addFormSuccess(form) {
-  return { type: ADD_FORM_SUCCESS, form };
-}
-
-function editFormSuccess(form) {
-  return { type: EDIT_FORM_SUCCESS, form };
-}
-
-export function activateFormSuccess(id) {
-  return { type: ACTIVATE_FORM_SUCCESS, id };
-}
-
-export class FormActions {
-  static inject() { return [FormApi, Store]; }
-
-  constructor(api, store) {
-    this._api = api;
-    this._store = store;
-  }
-
-  /**
-   * @desc Gets all the forms and dispatches that array in a success event
-   *
-   */
-  async loadForms() {
-    const forms = await this._api.get();
-
-    this._store.dispatch(loadFormsSuccess(forms));
-  }
-
-  /**
-   * @desc Saves the form and dispatches the new form in a success event
-   * @param {Form} form the form object
-   *
-   */
-  async saveForm(form) {
-    const serverVersion = await this._api.save(form);
-
-    form.id ? this._store.dispatch(editFormSuccess(serverVersion)) :
-      this._store.dispatch(addFormSuccess(serverVersion));
   }
 }
--- test/unit/domain/form/actions.spec.js
+++ test/unit/domain/form/actions.spec.js
@@ -1,19 +1,10 @@
-import { FormApi } from '../../../../src/api/form-api';
-import { Store } from 'aurelia-redux-plugin';
 import * as domain from '../../../../src/domain/index';
 import { setupSpy } from '../../jasmine-helpers';
-import using from 'jasmine-data-provider';
 
 describe('the form actions', () => {
-  var sut;
-  var storeSpy;
-  var apiSpy;
-
-  beforeEach(() => {
-    storeSpy = setupSpy('store', Store.prototype);
-    apiSpy = setupSpy('api', FormApi.prototype);
-    sut = new domain.FormActions(apiSpy, storeSpy);
-  });
+  let sut;
+  let storeSpy;
+  let apiSpy;
 
   it('create an action to request a form', () => {
     const id = 1;
@@ -100,49 +91,4 @@ describe('the form actions', () => {
       expect(actual.payload).toBe(payload);
     });
   });
-
-  it('loads all the forms', async done => {
-    const forms = [];
-
-    apiSpy.get.and.returnValue(forms);
-
-    await sut.loadForms();
-
-    expect(storeSpy.dispatch).toHaveBeenCalledWith({
-      type: 'LOAD_FORMS_SUCCESS', forms
-    });
-    expect(storeSpy.dispatch.calls.argsFor(0)[0].forms).toBe(forms);
-    done();
-  });
-
-  [ { id: null, type: 'ADD_FORM_SUCCESS'},
-    { id: undefined, type: 'ADD_FORM_SUCCESS'},
-    { id: '', type: 'ADD_FORM_SUCCESS'},
-    { id: 'a', type: 'EDIT_FORM_SUCCESS'}
-  ].forEach(data => {
-    it('adds the form if the id is not available', async done => {
-      const form = { id: data.id };
-      const serverForm = { };
-
-      apiSpy.save.and.returnValue(serverForm);
-
-      await sut.saveForm(form);
-
-      expect(apiSpy.save).toHaveBeenCalledWith(form);
-      expect(storeSpy.dispatch).toHaveBeenCalledWith({
-        type: data.type, form: serverForm
-      });
-      expect(storeSpy.dispatch.calls.argsFor(0)[0].form).toBe(serverForm);
-      done();
-    });
-  });
-
-  it('has a form activated action', () => {
-    const action = domain.activateFormSuccess('a');
-
-    expect(action).toEqual({
-      type: 'ACTIVATE_FORM_SUCCESS',
-      id: 'a'
-    });
-  })
 });
