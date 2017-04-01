417029f2c5890e0643898d43a6128a358e2576ed
Jeremy Zagorski <jmzagorski@gmail.com>
2017-04-01 13:44:54 -0400

    chore: add paraenthese around logic check in build

--- build/add-form-api.js
+++ build/add-form-api.js
@@ -6,7 +6,7 @@ module.exports = function (req, res, next) {
 
   var watching = req.path.lastIndexOf('/api/forms', 0) === 0;
 
-  if (req.method === 'POST' || req.method === 'PUT' && watching) {
+  if ((req.method === 'POST' || req.method === 'PUT') && watching) {
     if (req.body.api && req.body.template) {
       req.body.template = req.body.template
         .replace(/action=".*?"/, 'action="' + req.body.api + '"')
