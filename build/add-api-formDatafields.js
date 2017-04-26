fd5f1c81e2ba9893928e65e5f90cf0fa792877d0
Jeremy Zagorski <jmzagorski@gmail.com>
2017-04-26 14:33:35 -0400

    refactor(formdata): autoname form is applicable on server script

--- build/add-api-formDatafields.js
+++ build/add-api-formDatafields.js
@@ -1,9 +1,18 @@
+var RandExp = require('randexp');
+
 module.exports = function(router) {
 
   return function (req, res, next) {
     var watching = req.path.indexOf('forms/data', 0) !== -1;
 
     if (req.method === 'POST' && watching) {
+
+      var form = router.db.get('forms').getById(req.body.formId).value();
+
+      if (form.autoname) {
+        req.body.name = new RandExp(form.autoname).gen();
+      }
+
       req.body.saved = new Date();
     }
 
